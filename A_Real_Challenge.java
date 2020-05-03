3ab0432696ae50c350f53f5f5a511e7f385d8526
Esraa Farhat <36270858+EsraaFarhat@users.noreply.github.com>
2020-05-03 23:58:12 +0200

    Add files via upload

---
+++ A_Real_Challenge.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class A_Real_Challenge {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		double a = s.nextDouble();
+		System.out.println(Math.sqrt(a) * 4);
+
+	}
+
+}
