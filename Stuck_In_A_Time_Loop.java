225873140ae159770b76379f138febc305970e8b
Esraa Farhat <36270858+EsraaFarhat@users.noreply.github.com>
2020-05-25 05:17:22 +0200

    Add files via upload

---
+++ Stuck_In_A_Time_Loop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Stuck_In_A_Time_Loop {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		
+		for(int i=1; i<=N; i++) {
+			System.out.println(i + " Abracadabra");
+		}
+
+	}
+
+}
