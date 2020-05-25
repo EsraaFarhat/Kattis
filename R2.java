1c977c331b5b5a46abcc55b3ef0493ba780607d6
Esraa Farhat <36270858+EsraaFarhat@users.noreply.github.com>
2020-05-25 05:47:32 +0200

    Add files via upload

---
+++ R2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class R2 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int R1 = s.nextInt();
+		int S = s.nextInt();
+		
+		System.out.println((2*S) - R1);
+
+	}
+
+}
