e303b63d10367cd1f93f8e922ec626f397b8a817
Esraa Farhat <36270858+EsraaFarhat@users.noreply.github.com>
2020-05-25 05:29:14 +0200

    Add files via upload

---
+++ Solving_for_Carrots.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solving_for_Carrots {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		int P = s.nextInt();
+		
+		for(int i=0;i<N;i++) {
+			String contestant = s.next();
+		}
+		
+		System.out.println(P);
+
+	}
+
+}
