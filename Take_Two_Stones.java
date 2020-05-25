bdb3838a6826cbe92823dc43a27a21a5ef162ed0
Esraa Farhat <36270858+EsraaFarhat@users.noreply.github.com>
2020-05-25 05:11:56 +0200

    Add files via upload

---
+++ Take_Two_Stones.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Take_Two_Stones {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		long N = s.nextLong();
+		if(N%2 == 0) System.out.println("Bob");
+		else System.out.println("Alice");
+
+	}
+
+}
